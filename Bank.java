007a1f2d588487a2f2adae42d0d270dd2a4616c2
Ajayilla <114133881+Teekshansri@users.noreply.github.com>
2022-09-28 12:30:07 +0530

    Add files via upload

---
+++ Bank.java
@@ -0,0 +1,12 @@
+// bank account .java 
+class bank account {
+	static string bank name ;
+	static string branch name ;
+	static string if sc ;
+	long acc name ;
+	string acc name ;
+	double balance ;
+}
+	
+	
+{
